9964b90f999f714d667808a89aeb60628688c10f
King Keys Admin <kingkeysshop@gmail.com>
2026-06-02 10:36:48 -0500

    fix: update Windows icon to correct 4-pane logo

--- src/modules/common/icons/windows.tsx
+++ src/modules/common/icons/windows.tsx
@@ -8,10 +8,7 @@ const Windows: React.FC<IconProps> = ({
 }) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...attributes}>
-      <path d="M3 5.5L11 4.5V11.5H3V5.5Z" fill="currentColor" />
-      <path d="M3 18.5L11 19.5V12.5H3V18.5Z" fill="currentColor" />
-      <path d="M12 4.5L21 3V11.5H12V4.5Z" fill="currentColor" />
-      <path d="M12 12.5H21V21L12 19.5V12.5Z" fill="currentColor" />
+      <path fill={color} d="M0 0h11.377v11.372H0Zm12.623 0H24v11.372H12.623ZM0 12.623h11.377V24H0Zm12.623 0H24V24H12.623" />
     </svg>
   )
 }
